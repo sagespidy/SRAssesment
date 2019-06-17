62cb80a3ca5d165ae885d58cde6c25aeacc42988
EC2 Default User <ec2-user@ip-172-31-81-201.ec2.internal>
2019-06-17 02:42:30 +0000

    with docker

--- terraform/modules/ec2-ansible-deploy/main.tf
+++ terraform/modules/ec2-ansible-deploy/main.tf
@@ -1,19 +1,50 @@
+resource "aws_security_group" "nginx-sg" {
+  name = "nginx-sg" 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "tls_private_key" "test-key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "${var.key_name}"
+  public_key = "${tls_private_key.test-key.public_key_openssh}"
+}
+
 resource "aws_instance" "my-test-instance" {
   ami             = "${data.aws_ami.centos.id}"
   instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.nginx-sg.name}"]
+  key_name      = "${aws_key_pair.generated_key.key_name}"
 
   tags = {
     Name = "test-instance"
   }
-
-    security_group_ids = ["${aws_security_group.test-instance.id}"]
-    associate_public_ip_address = true
-
-    # We're assuming there's a key with this name already
-    key_name = "AdminSSH"
-
+  
     # This is where we configure the instance with ansible-playbook
     provisioner "local-exec" {
-        command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u centos --private-key ./AdminSSH.pem -i '${aws_instance.test-instance.public_ip},' playbook.yml"
+        command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u centos --private-key ${aws_key_pair.generated_key.key_name}.pem -i '${aws_instance.my-test-instance.public_ip},' playbook.yml"
     }
 }
